8e1eb96ad5074d8cd92ae872196f0387a40df3eb
fabiomorais <fabio.jorge@gmail.com>
2018-09-23 10:16:00 -0300

    Avaliações por matricula e grupo

--- code/agrupaAvaliacoes.R
+++ code/agrupaAvaliacoes.R
@@ -35,27 +35,40 @@ avaliacoes <- avaliacoes %>% distinct() %>% group_by(id, matricula) %>%
 avaliacoes %>% distinct() %>% group_by(id, matricula) %>% 
   summarise(avaliacao_diff = n()) %>% filter(avaliacao_diff > 1) %>% nrow()
 
-head(avaliacoes)
-# Coletando matricula e grupo
-mduplicada <- dff %>% select(matricula, grupo_avaliando) %>% 
+# Coletando matricula e grupo duplicadas
+duplicadas <- dff %>% select(matricula, grupo_avaliando) %>% 
   filter(!is.na(matricula)) %>% distinct() %>% 
   group_by(matricula) %>% summarise(ngrupos = n()) %>% 
   filter(ngrupos > 1)
+duplicadas
 
 # Quais os grupos das matrículas duplicadas
 dff %>% select(matricula, grupo_avaliando) %>% 
   filter(!is.na(matricula)) %>% distinct() %>%
-  filter(matricula %in% mduplicada$matricula) %>% 
+  filter(matricula %in% duplicadas$matricula) %>% 
   arrange(matricula, grupo_avaliando)
 
 # Quantas vezes uma matricula duplicada está associada a um grupo
 dff %>% select(matricula, grupo_avaliando, id) %>% 
   filter(!is.na(matricula)) %>% 
-  filter(matricula %in% mduplicada$matricula) %>% 
+  filter(matricula %in% duplicadas$matricula) %>% 
+  group_by(matricula, grupo_avaliando) %>% 
+  summarise(noccor = n()) 
+
+# Selecionando matricula e grupo para cada aluno (removendo duplicações)
+grupos <- dff %>% select(matricula, grupo_avaliando, id) %>% 
+  filter(!is.na(matricula)) %>% 
   group_by(matricula, grupo_avaliando) %>% 
   summarise(noccor = n()) %>% 
   top_n(1, noccor)
-  
 
-  # Escreve dataframe em arquivo
+
+head(avaliacoes)
+head(grupos)
+
+# Fazendo join ente df com matriculas e grupos e df com avaliações por matricula
+avaliacoes <- avaliacoes %>% left_join(grupos, by = "matricula") %>% 
+  select(id, matricula, grupo = grupo_avaliando, avaliacao)
+
+# Escreve dataframe em arquivo
 write.csv(avaliacoes, file = "data/3-avaliacao-humana/avaliacoes-20180912.csv", row.names = F)
