d1ae1fff65f7112d9d7562b6bdc531f5e717bf68
fabiomorais <fabio.jorge@gmail.com>
2018-09-26 21:23:47 -0300

    Exemplo de normalização de escala de valores

---
+++ code/normalizacao.R
@@ -0,0 +1,16 @@
+library(dplyr)
+
+# Dataframe 1 exemplo
+exemplo_1 <- data.frame(exemplo = 1, metrica = letters[1:10], valor = sample(1:100, 10))
+
+# Dataframe 2 exemplo
+exemplo_2 <- data.frame(exemplo = 2, metrica = letters[1:10], valor = sample(1:10, 10))
+
+# Dois dataframes com diferentes escalas (1 a 100) e (1 a 10)
+# normalizar em escala de 0 a 1 = (valor - min) / (max - min)
+exemplo_1_normalizado = exemplo_1 %>% mutate(valor_normalizado = (valor - min(valor)) / (max(valor) - min(valor)))
+exemplo_2_normalizado = exemplo_2 %>% mutate(valor_normalizado = (valor - min(valor)) / (max(valor) - min(valor)))
+
+# O maior valor dos dataframes normalizados é igual a 1 e o menor é igual a zero
+summary(exemplo_1_normalizado$valor_normalizado)
+summary(exemplo_2_normalizado$valor_normalizado)
